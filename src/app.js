ca42b20e3da681b05fc421483df4f1ebdcce1425
杨坤海 <384037191@qq.com>
2019-07-31 08:02:31 +0800

    refactor

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import {
   setDisplayName,
   withHandlers,
   lifecycle,
-  withState,
+  withState
 } from "recompose";
 import QueryString from "query-string";
 import {
@@ -12,7 +12,7 @@ import {
   DragTabList,
   DragTab,
   PanelList,
-  Panel,
+  Panel
 } from "react-tabtab/lib/";
 import { simpleSwitch } from "react-tabtab/lib/helpers/move";
 import * as customStyle from "react-tabtab/lib/themes/material-design";
@@ -21,21 +21,15 @@ import Search from "./search";
 import logo from "../static/logo-white.png";
 import style from "./index.less";
 import Header from "./header";
+import { links, tabs } from "./config";
 
-const keywordLength = (str) => str && str.trim() && str.trim().length;
+const keywordLength = str => str && str.trim() && str.trim().length;
 
 const createPanel = (url, title) => (
   <Panel>
-    <iframe
-      title={title}
-      src={url}
-      width="100%"
-      height="800"
-      frameborder="0"
-    />
+    <iframe title={title} src={url} width="100%" height="800" frameborder="0" />
   </Panel>
 );
-
 
 export default compose(
   setDisplayName(__filename),
@@ -91,131 +85,88 @@ export default compose(
       window.location.search = `?q=${q}`;
       onSearch(q);
     }
-  }),
-)(({
-  onSearch, 
-  keyword, 
-  setKeyword,
-  height,
-  searching,
-  activeIndex,
-  setActiveIndex,
-  handleTabSequenceChange,
- }) =>
-  keyword ? (
-    <div>
-      <div className={style.Header}>
-        <Header onSearch={onSearch} keyword={keyword} setKeyword={setKeyword} />
-      </div>
-      <div className={style.Content} style={{ minHeight: height || 420 }}>
-        <div className={style.LinkWrap}>
-          <a
-            title="Github"
-            href={`https://github.com/search?q=${keyword}`}
-            target="blank"
-            className={style.Link}
-          >
-            Github
-          </a>
-          <a
-            title="知乎"
-            href={`https://www.zhihu.com/search?type=content&q=${keyword}`}
-            target="blank"
-            className={style.Link}
-          >
-            知乎
-          </a>
-          <a
-            title="微信"
-            href={`https://weixin.sogou.com/weixin?p=01030402&query=${keyword}&type=2&ie=utf8`}
-            target="blank"
-            className={style.Link}
-          >
-            微信
-          </a>
-          <a
-            title="Google"
-            href={`https://www.google.com.hk/search?safe=strict&source=hp&q=${keyword}&oq=${keyword}`}
-            target="blank"
-            className={style.Link}
-          >
-            Google
-          </a>
-          <a
-            title="简书"
-            href={`https://www.jianshu.com/search?q=${keyword}`}
-            target="blank"
-            className={style.Link}
-          >
-            简书
-          </a>
-          <a
-            title="掘金"
-            href={`https://juejin.im/search?query=${keyword}&type=all`}
-            target="blank"
-            className={style.Link}
-          >
-            掘金
-          </a>
+  })
+)(
+  ({
+    onSearch,
+    keyword,
+    setKeyword,
+    height,
+    searching,
+    activeIndex,
+    setActiveIndex,
+    handleTabSequenceChange
+  }) =>
+    keyword ? (
+      <div>
+        <div className={style.Header}>
+          <Header
+            onSearch={onSearch}
+            keyword={keyword}
+            setKeyword={setKeyword}
+          />
         </div>
-        {searching && (
-          <Tabs
-            activeIndex={activeIndex}
-            onTabChange={setActiveIndex}
-            onTabSequenceChange={handleTabSequenceChange}
-            customStyle={customStyle}
-            showModalButton={false}
-          >
-            <DragTabList>
-              <DragTab>多吉</DragTab>
-              <DragTab>百度</DragTab>
-              <DragTab>微博</DragTab>
-              <DragTab>SegmentFault</DragTab>
-              <DragTab>翻译</DragTab>
-              <DragTab>搜课</DragTab>
-              <DragTab>Yarn</DragTab>
-            </DragTabList>
-            <PanelList>
-              {createPanel(`https://dogedoge.com/results?q=${keyword}`, "多吉")}
-              {createPanel(`https://www.baidu.com/s?wd=${keyword}`, "百度")}
-
-              {createPanel(
-                `https://s.weibo.com/weibo/${keyword}?topnav=1&wvr=6`,
-                "微博"
-              )}
-              {createPanel(
-                `https://segmentfault.com/search?q=${keyword}`,
-                "SegmentFault"
-              )}
-              {createPanel(`https://fanyi.baidu.com/#en/zh/${keyword}`, "翻译")}
-
-              {createPanel(`http://souke.xyz/result#q=${keyword}`, "搜课")}
-
-              {createPanel(
-                `https://www.yarnpkg.com/zh-Hans/packages?q=${keyword}`,
-                "yarn"
-              )}
-            </PanelList>
-          </Tabs>
-        )}
-      </div>
-      <footer className={style.Footer}>
-        © 2019 Research Created by Andy | 蜀ICP备18015889号-1
-      </footer>
-    </div>
-  ) : (
-    <div
-      className={style.SearchPage}
-      style={{
-        backgroundImage: "url(https://cn.bing.com/th?id=OHR.TrilliumLake_EN-CN1200736040_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp)"
-      }}
-    >
-      <div className={style.SearchBar}>
-        <div className={style.LogoWrap}>
-          <img className={style.Logo} src={logo} alt="" />
+        <div className={style.Content} style={{ minHeight: height || 420 }}>
+          <div className={style.LinkWrap}>
+            {links(keyword).map(({ link, title }) => (
+              <a
+                title={title}
+                key={link}
+                href={link}
+                target="blank"
+                className={style.Link}
+              >
+                {title}
+              </a>
+            ))}
+          </div>
+          {searching && (
+            <Tabs
+              activeIndex={activeIndex}
+              onTabChange={setActiveIndex}
+              onTabSequenceChange={handleTabSequenceChange}
+              customStyle={customStyle}
+              showModalButton={false}
+            >
+              <DragTabList>
+                {tabs().map(({ title }) => (
+                  <DragTab key={title}>{title}</DragTab>
+                ))}
+              </DragTabList>
+              <PanelList>
+                {tabs(keyword).map(({ title, link }) => (
+                  <Panel key={link}>
+                    <iframe
+                      title={title}
+                      src={link}
+                      width="100%"
+                      height="800"
+                      frameBorder="0"
+                    />
+                  </Panel>
+                ))}
+              </PanelList>
+            </Tabs>
+          )}
         </div>
-        <Search onSearch={onSearch} placeholder="javascript" />
+        <footer className={style.Footer}>
+          © 2019 Research Created by Andy | 蜀ICP备18015889号-1
+        </footer>
       </div>
-    </div>
-  )
+    ) : (
+      <div
+        className={style.SearchPage}
+        style={{
+          backgroundImage:
+            "url(https://cn.bing.com/th?id=OHR.TrilliumLake_EN-CN1200736040_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp)"
+        }}
+      >
+        <div className={style.SearchBar}>
+          <div className={style.LogoWrap}>
+            <img className={style.Logo} src={logo} alt="" />
+          </div>
+          <Search onSearch={onSearch} placeholder="javascript" />
+        </div>
+      </div>
+    )
 );
---
+++ src/config.js
@@ -0,0 +1,75 @@
+export const links = keyword => [
+  {
+    title: "Github",
+    link: `https://github.com/search?q=${keyword}`
+  },
+  {
+    title: "知乎",
+    link: `https://www.zhihu.com/search?type=content&q=${keyword}`
+  },
+  {
+    title: "微信",
+    link: `https://weixin.sogou.com/weixin?p=01030402&query=${keyword}&type=2&ie=utf8`
+  },
+  {
+    title: "Google",
+    link: `https://www.google.com.hk/search?safe=strict&source=hp&q=${keyword}&oq=${keyword}`
+  },
+  {
+    title: "简书",
+    link: `https://www.jianshu.com/search?q=${keyword}`
+  },
+  {
+    title: "掘金",
+    link: `https://juejin.im/search?query=${keyword}&type=all`
+  },
+  {
+    title: "NPM",
+    link: `https://www.npmjs.com/search?q=${keyword}`
+  },
+  {
+    title: "Stackoverflow",
+    link: `https://stackoverflow.com/search?q=${keyword}`
+  }
+];
+
+
+export const tabs = (keyword)=>[
+  {
+    title: "多吉",
+    link: `https://dogedoge.com/results?q=${keyword}`
+  },
+  {
+    title: "百度",
+    link: `https://www.baidu.com/s?wd=${keyword}`
+  },
+  {
+    title: "微博",
+    link: `https://s.weibo.com/weibo/${keyword}?topnav=1&wvr=6`,
+  },
+  {
+    title: "SegmentFault",
+    link:  `https://segmentfault.com/search?q=${keyword}`,
+  },
+  {
+    title: "Yarn",
+    link: `https://www.yarnpkg.com/zh-Hans/packages?q=${keyword}`
+  },
+  {
+    title: "Bilibili",
+    link: `https://search.bilibili.com/all?keyword=${keyword}`
+  },
+  
+  {
+    title: "搜课",
+    link: `http://souke.xyz/result#q=${keyword}`
+  },
+  
+];
+
+              
+               
+
+          
+
+            
