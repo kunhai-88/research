83e272d7f1b52afcbe6bd07858c53a72c89858d8
杨坤海 <384037191@qq.com>
2019-08-02 08:20:19 +0800

    优化性能

--- webpack.js
+++ webpack.js
@@ -1,127 +1,119 @@
-const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const path = require('path');
+const autoprefixer = require("autoprefixer");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const path = require("path");
 
-
-const DEBUG = process.env.mode !== 'production';
-const PROD = !DEBUG;
+const DEBUG = process.env.mode !== "production";
 
 const autoprefixerConfig = {
   browsers: [
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 2.3',
-    'bb >= 10',
-  ],
+    "ie_mob >= 10",
+    "ff >= 30",
+    "chrome >= 34",
+    "safari >= 7",
+    "opera >= 23",
+    "ios >= 7",
+    "android >= 2.3",
+    "bb >= 10"
+  ]
 };
 
 const plugins = [
   new CleanWebpackPlugin(),
   new CopyPlugin([
-    { from: path.resolve(__dirname, './public'), to: path.resolve(__dirname, './dist') },
+    {
+      from: path.resolve(__dirname, "./public"),
+      to: path.resolve(__dirname, "./dist")
+    }
   ]),
   new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, './src/index.html'),
-    hash: true,
+    template: path.resolve(__dirname, "./src/index.html"),
+    hash: true
   }),
   new webpack.HotModuleReplacementPlugin(),
-  new webpack.NamedModulesPlugin(),
-  new MiniCssExtractPlugin({
-    filename: '[name].[hash].css',
-    chunkFilename: '[name].[hash].css',
-  }),
+  new webpack.NamedModulesPlugin()
 ];
 
 if (DEBUG) {
-  plugins.push(new webpack.SourceMapDevToolPlugin({
-    filename: '[name].js.map',
-  }));
-  // plugins.push(new BundleAnalyzerPlugin());
+  plugins.push(
+    new webpack.SourceMapDevToolPlugin({
+      filename: "[name].js.map"
+    })
+  );
+  
 }
+plugins.push(new BundleAnalyzerPlugin());
 
 const cssLoaderWithModule = {
   importLoaders: 1,
   modules: true,
-  localIdentName: DEBUG ? '[name]_[local]--[hash:base64:5]' : '[hash:base64:5]',
-  sourceMap: DEBUG,
+  localIdentName: DEBUG ? "[name]_[local]--[hash:base64:5]" : "[hash:base64:5]",
+  sourceMap: DEBUG
 };
 
 const cssLoaderWithoutModule = {
-  sourceMap: DEBUG,
+  sourceMap: DEBUG
 };
 
-
 const postcssLoader = {
-  loader: 'postcss-loader',
+  loader: "postcss-loader",
   options: {
-    ident: 'postcss',
+    ident: "postcss",
     sourceMap: DEBUG,
-    plugins: () => [
-      autoprefixer(autoprefixerConfig),
-    ],
-  },
+    plugins: () => [autoprefixer(autoprefixerConfig)]
+  }
 };
 
 const config = {
   entry: {
-    app: [
-      path.resolve(__dirname, './src/index.js'),
-    ],
+    app: [path.resolve(__dirname, "./src/index.js")]
   },
 
   node: {
     __filename: true,
     __dirname: true,
-    module: 'empty',
-    net: 'empty',
-    fs: 'empty',
+    module: "empty",
+    net: "empty",
+    fs: "empty"
   },
 
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath:  '/',
-    filename: '[name].[hash].bundle.js',
-    chunkFilename: '[name].[hash].bundle.js',
-    globalObject: 'this',
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/",
+    filename: "[name].[hash].bundle.js",
+    chunkFilename: "[name].[hash].bundle.js",
+    globalObject: "this"
   },
   optimization: {
     runtimeChunk: {
-      name: 'manifest',
+      name: "manifest"
     },
-    minimizer: [
-      new UglifyJsPlugin(),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+    minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin({})]
   },
 
   cache: DEBUG,
-  devtool: DEBUG ? 'inline-source-map' : false,
+  devtool: DEBUG ? "inline-source-map" : false,
   devServer: {
-    contentBase: [path.resolve(__dirname, './dist')],
+    contentBase: [path.resolve(__dirname, "./dist")],
     inline: true,
     compress: true,
     port: 8000,
     hotOnly: true,
     open: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   mode: process.env.mode,
   resolve: {
-    extensions: ['.js', '.less'],
+    extensions: [".js", ".less"],
     alias: {
-      src: path.resolve(__dirname, './src'),
-      static: path.resolve(__dirname, './static'),
-    },
+      src: path.resolve(__dirname, "./src"),
+      static: path.resolve(__dirname, "./static")
+    }
   },
 
   module: {
@@ -130,86 +122,79 @@ const config = {
         test: /\.js$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               cacheDirectory: true,
               babelrc: false,
-              presets: [
-                '@babel/preset-react',
-                '@babel/preset-env',
-              ],
+              presets: ["@babel/preset-react", "@babel/preset-env"],
               plugins: [
-                'transform-class-properties',
-                'lodash',
-                ['@babel/plugin-proposal-decorators', { legacy: true }]
-              ],
-            },
-          },
-        ],
+                "transform-class-properties",
+                ["@babel/plugin-proposal-decorators", { legacy: true }]
+              ]
+            }
+          }
+        ]
       },
       {
         test: /[^_]\.less$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            // you can specify a publicPath here
-            // by default it use publicPath in webpackOptions.output
-            publicPath: './',
+        use: [
+          {
+            loader: "style-loader"
           },
-        }, {
-          loader: 'css-loader', // translates CSS into CommonJS
-          options: cssLoaderWithModule,
-        }, postcssLoader, {
-          loader: 'less-loader', // compiles Less to CSS
-          options: { javascriptEnabled: true },
-        }],
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: cssLoaderWithModule
+          },
+          postcssLoader,
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: { javascriptEnabled: true }
+          }
+        ]
       },
       {
         test: /_\.less$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            // you can specify a publicPath here
-            // by default it use publicPath in webpackOptions.output
-            publicPath: './',
+        use: [
+          {
+            loader: "style-loader"
           },
-        }, {
-          loader: 'css-loader', // translates CSS into CommonJS
-          options: cssLoaderWithoutModule,
-        }, postcssLoader, {
-          loader: 'less-loader', // compiles Less to CSS
-          options: { javascriptEnabled: true },
-        }],
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: cssLoaderWithoutModule
+          },
+          postcssLoader,
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: { javascriptEnabled: true }
+          }
+        ]
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: './',
-            },
+            loader: "style-loader"
           },
-          'css-loader',
-        ],
+          "css-loader"
+        ]
       },
       {
         test: /\.(png|jpg|jpeg|svg)/,
-        use: 'file-loader',
-      }, {
+        use: "file-loader"
+      },
+      {
         test: /\.(woff|eot|ttf)/,
-        use: 'url-loader',
-      }, {
+        use: "url-loader"
+      },
+      {
         test: /\.(xlsx?|pdf)/,
-        use: 'file-loader',
+        use: "file-loader"
       }
-    ],
+    ]
   },
 
   plugins,
-  profile: true,
+  profile: true
 };
 
 module.exports = config;
